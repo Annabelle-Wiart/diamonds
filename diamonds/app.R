e4e69e98ae341252ab43936bd251af568a2b3e1b
Annabelle Wiart <Anna.Wiart@hotmail.com>
2025-12-07 19:57:50 +0100

    Personnalisation de l'application
    Pourquoi:
    - Ajouter une esthétique à l'application
    
    Quoi:
    - Ajout d'un thème Bootswatch

--- diamonds/app.R
+++ diamonds/app.R
@@ -2,7 +2,11 @@ library(shiny)
 library(bslib)
 
 ui <- bslib::page_fluid(
-  titlePanel("Exploration des Diamants")
+  titlePanel("Exploration des Diamants"),
+  theme = bs_theme(
+    version = 5,
+    bootswatch = "minty"
+  )
 )
 
 server <- function(input, output, session){
