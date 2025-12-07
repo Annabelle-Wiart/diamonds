93060beee7decee9ba6dc81ba284707e8e415923
Annabelle Wiart <Anna.Wiart@hotmail.com>
2025-12-07 20:16:16 +0100

    Premier élément visuel d'interface graphique
    Pourquoi:
    - Initialiser le bouton qui servira à changer la couleur du graphique en rose ou en noir
    
    Quoi:
    - Ajout d'un bouton d'option de type radio

--- diamonds/app.R
+++ diamonds/app.R
@@ -1,15 +1,31 @@
 library(shiny)
 library(bslib)
+library(dplyr)
+library(ggplot2)
+library(plotly)
 
 ui <- bslib::page_fluid(
   titlePanel("Exploration des Diamants"),
   theme = bs_theme(
     version = 5,
     bootswatch = "minty"
+  ),
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(inputId = "radio", 
+                   label = "Colorier les points en rose ?",
+                   choices = list("Oui" = 1, "Non" = 2), 
+                   selected = 1)
+      
+    ),
+    mainPanel(
+      
+    )
   )
 )
 
 server <- function(input, output, session){
+  
   
 }
   
